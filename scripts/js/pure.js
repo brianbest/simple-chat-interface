9e2802895e7415e738bd496a42caf46ecf7386c0
Brian best <brian.brianb64@gmail.com>
2014-10-01 13:19:40 -0300

    Comments added

--- scripts/js/pure.js
+++ scripts/js/pure.js
@@ -1,24 +1,29 @@
 //Pure Javascript version
 
-
+  //Take whats in #chat_area, placein element and append to #msg_area
+  //-------------------------------
   function sendMessage(event){
     if (event.keyCode === 13){
       var msg = document.getElementById('chat_area').value;
-      console.log(msg+'That msg');
       var msg_holder = document.createElement('P');
       var msg_textNode = document.createTextNode(msg);
       msg_holder.appendChild(msg_textNode);
       document.getElementById('msg_area').appendChild(msg_holder);
-      document.getElementById('chat_area').value = "";
+
+      //Clear chat field when complete
+      document.getElementById('chat_area').value = "first";
     }
   }
 
+  //Only once the chat area is activated do we listen for a key up event
+  //-------------------------------
   function chatClicked(){
       console.log('clicked');
-      document.addEventListener("keydown", function(){sendMessage(event);}, false);
+      document.addEventListener("keyup", function(){sendMessage(event);}, false);
   }
 
 
 
-
+  //Listen for chat area to be activated
+  //-------------------------------
   document.getElementById('chat_area').addEventListener("click", function(){chatClicked();}, false);
