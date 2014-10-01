bbe09b3006f0fea1b784b244b4d3183f437644d2
Brian best <brian.brianb64@gmail.com>
2014-10-01 16:45:40 -0300

    font implement,and prepend

--- scripts/js/pure.js
+++ scripts/js/pure.js
@@ -3,12 +3,16 @@
   //Take whats in #chat_area, placein element and append to #msg_area
   //-------------------------------
   function sendMessage(event){
+    //Check if Enter was clicked
     if (event.keyCode === 13){
       var msg = document.getElementById('chat_area').value;
       var msg_holder = document.createElement('P');
       var msg_textNode = document.createTextNode(msg);
       msg_holder.appendChild(msg_textNode);
-      document.getElementById('msg_area').appendChild(msg_holder);
+
+      //Prepend created object to top of the stack
+      var pastChild = document.getElementById('msg_area').firstChild;
+      document.getElementById('msg_area').insertBefore(msg_holder,pastChild);
 
       //Clear chat field when complete
       document.getElementById('chat_area').value = "";
